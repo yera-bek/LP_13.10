6d238069235936e37993d95d4ef089ee6196de91
yera.bek <kalatoskin@gmail.com>
2022-10-14 01:38:55 +0600

    first lesson

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // System.Console.WriteLine("Введите целое число: ");
+// // int num = Convert.ToInt32(Console.ReadLine());
+
+// // // string str = Console.ReadLine();
+// // // int n = Convert.ToInt32(str);
+
+// // int square = num * num;
+// // Console.WriteLine($"Квадрат числа {num} = {square}");
+
+// // bool res = num > 5 && num < 10; // true - fals
+// // if (res)
+// // {
+
+// // }
+
+// Console.WriteLine("Write number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("write number 2");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (b*b == a)
+// {
+//     Console.Write ("Yep");
+// }
+// else
+// {
+//     Console.Write("Nope");
+// }
