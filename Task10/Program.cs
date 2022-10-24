37c63b3ed2f7201d3785c76dbdcbb1e5b409a28b
yera.bek <kalatoskin@gmail.com>
2022-10-25 04:21:21 +0600

    task10

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа. 
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления)
+
+Console.WriteLine("Enter a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = RemoveFirstAndLastDigit(num);
+Console.WriteLine(result);
+
+int RemoveFirstAndLastDigit(int number)
+{
+    int firstDigit = number / 100;
+    int secondDigit = number % 10;
+   return (num - (firstDigit * 100 + secondDigit)) / 10;
+}
