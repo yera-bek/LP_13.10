0d15c00acbb8b4ff9e6b80229480d94e4f7ca360
yera.bek <kalatoskin@gmail.com>
2022-11-01 00:48:49 +0600

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Write a programm
+1. Takes a number (N) as input
+2. Produces a spreadsheet of cubes of numbers from 1 to N
+*/
+
+Console.WriteLine("Input the number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine($"{i} | {Math.Pow(i, 3)}");
+}
